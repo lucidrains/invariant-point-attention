fix quaternions bug

invariant_point_attention/invariant_point_attention.py
@@ -320,7 +320,7 @@ class IPATransformer(nn.Module):
             quaternion_update, translation_update = to_update(x).chunk(2, dim = -1)
             quaternion_update = F.pad(quaternion_update, (1, 0), value = 1.)
 
-            quaternions = quaternions + quaternion_multiply(quaternions, quaternion_update)
+            quaternions = quaternion_multiply(quaternions, quaternion_update)
             translations = translations + einsum('b n c, b n c r -> b n r', translation_update, rotations)
 
         if not self.predict_points: